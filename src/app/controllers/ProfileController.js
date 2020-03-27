5f89e4276a78714c5bcaf0268d188268fa179334
Márcio França Lima <marciofrancalima@hotmail.com>
2020-03-27 18:34:51 -0300

    chore: Add pagination to the profile data listing

--- src/app/controllers/ProfileController.js
+++ src/app/controllers/ProfileController.js
@@ -1,5 +1,8 @@
 const connection = require('../../database');
 
+const PAGE = 1;
+const LIMIT = 10;
+
 class ProfileController {
   async index(req, res) {
     const ong_id = req.headers.authorization;
@@ -9,9 +12,17 @@ class ProfileController {
       return res.status(400).json({ error: 'ONG id not informed' });
     }
 
+    const { page = PAGE } = req.query;
+
+    const [count] = await connection('incidents').count();
+
     const incidents = await connection('incidents')
+      .limit(LIMIT)
+      .offset((page - 1) * LIMIT)
       .where('ong_id', ong_id)
-      .select('*');
+      .select(['*']);
+
+    res.header('X_Total-Count', count['count(*)']);
 
     return res.status(200).json(incidents);
   }
